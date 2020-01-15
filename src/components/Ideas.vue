5300d3323302970d19418b8f215a5b988853385e
Alex Simisi <smsxander@gmail.com>
2020-01-15 23:52:31 +0100

    creation de deux colonnes pour effectuer le drag and drop

--- src/components/Ideas.vue
+++ src/components/Ideas.vue
@@ -1,9 +1,25 @@
 <template>
   <div>
-    <h2>Ideas</h2>
-    <draggable>
-      <Idea v-for="idea in allIdeas" :key="idea.id" :item="idea" />
-    </draggable>
+    <div>
+      <h1>Ideas</h1>
+      <draggable v-model="allIdeas" class="ideas" group="ideas">
+        <Idea v-for="idea in allIdeas" :key="idea.id" :item="idea" />
+      </draggable>
+    </div>
+    <div class="destination">
+      <div>
+        <h1>Courses</h1>
+        <draggable v-model="courses" class="ideas" group="ideas">
+          <Idea v-for="course in courses" :key="course.id" :item="course" />
+        </draggable>
+      </div>
+      <div>
+        <h1>Paper work</h1>
+        <draggable v-model="paperwork" class="ideas" group="ideas">
+          <Idea v-for="pw in paperwork" :key="pw.id" :item="pw" />
+        </draggable>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -18,8 +34,31 @@ export default {
     Idea
   },
   computed: {
-    allIdeas() {
-      return this.$store.getters.allIdeas;
+    //allIdeas() {
+    allIdeas: {
+      //return this.$store.getters.allIdeas;
+      get() {
+        return this.$store.getters.allIdeas;
+      },
+      set(value) {
+        return this.$store.dispatch("saveIdeasOrdering", value);
+      }
+    },
+    courses: {
+      get() {
+        return this.$store.getters.courses;
+      },
+      set(value) {
+        return this.$store.dispatch("saveCoursesOrdering", value);
+      }
+    },
+    paperwork: {
+      get() {
+        return this.$store.getters.paperwork;
+      },
+      set(value) {
+        return this.$store.dispatch("savePaperWorkOrdering", value);
+      }
     }
   }
 };
@@ -30,5 +69,15 @@ li {
   list-style: none;
   cursor: pointer;
   padding: 5px;
+}
+.ideas {
+  display: flex;
+  flex-direction: column;
+  flex-wrap: wrap;
+  align-content: center;
+}
+.destination {
+  display: flex;
+  justify-content: space-evenly;
 }
 </style>
--- src/store/index.js
+++ src/store/index.js
@@ -6,7 +6,9 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-    ideas: {}
+    ideas: {},
+    courses: [],
+    paperwork: []
   },
   // commit mutations
   mutations: {
@@ -14,19 +16,51 @@ export default new Vuex.Store({
       const id = Date.now();
       state.ideas[id] = payload;
       state.ideas = { ...state.ideas };
+    },
+    SAVE_IDEAS_ORDERING(state, payload) {
+      state.ideas = { ...payload };
+    },
+    SAVE_COURSES_ORDERING(state, payload) {
+      state.courses = [...payload];
+    },
+    SAVE_PAPERWORK_ORDERING(state, payload) {
+      state.paperwork = [...payload];
     }
   },
   // dispatch actions
   actions: {
     createIdea({ commit }, payload) {
-      console.log("actions / createIdea");
       commit("CREATE_IDEAS", payload);
+    },
+    saveIdeasOrdering({ commit }, payload) {
+      const ideas = {};
+      //debugger;
+      payload.map(idea => {
+        return (ideas[idea.id] = idea);
+      });
+      commit("SAVE_IDEAS_ORDERING", ideas);
+    },
+    saveCoursesOrdering({ commit }, payload) {
+      commit("SAVE_COURSES_ORDERING", payload);
+    },
+    savePaperWorkOrdering({ commit }, payload) {
+      commit("SAVE_PAPERWORK_ORDERING", payload);
     }
   },
   getters: {
     allIdeas(state) {
-      const allIdeas = Object.keys(state.ideas).map(key => state.ideas[key]);
+      const allIdeas = Object.keys(state.ideas).map(key => {
+        const idea = state.ideas[key];
+        idea.id = key;
+        return idea;
+      });
       return allIdeas;
+    },
+    courses(state) {
+      return state.courses;
+    },
+    paperwork(state) {
+      return state.paperwork;
     }
   }
 });
